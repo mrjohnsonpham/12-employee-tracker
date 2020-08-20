8725220397399bfe5f964e980ab667d4a2548d76
Johnson Pham <mrjohnsonpham@gmail.com>
2020-08-20 16:35:33 -0400

    Added functions for department, role, and add employee

--- server.js
+++ server.js
@@ -3,70 +3,178 @@ const inquirer = require("inquirer");
 const table = require("console.table");
 const promisemysql = require("promise-mysql");
 
-
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "Iloverocky21!",
-    database: "employee_tracker_db"
-  });
+  host: "localhost",
 
-  // connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-  });
+  // Your port; if not 3306
+  port: 3306,
 
-  // function which prompts the user for what action they should take
-function start() {
-    inquirer
-      .prompt({
-        name: "menu",
-        type: "list",
-        message: "Please choose one of the following:",
-        choices: [
-            "View all Employees",
-            "View all Departments",
-            "View all Roles", 
-            "Add Employees",
-            "Add Department",
-            "Add Role",
-            "Update Employee Role",
-            "Exit"
-        ]
-      })
-      .then(function(answer) {
-        switch(answer.choices){
-            case "View All Employees":
-            employeeView();
-            break;
+  // Your username
+  user: "root",
 
-            case "Exit":
-                    exit();
-                    break;
+  // Your password
+  password: "Iloverocky21!",
+  database: "employee_tracker_db",
+});
 
-        }
-  })
-}
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+// function which prompts the user for what action they should take
+const start = () => {
+  inquirer
+    .prompt({
+      name: "menu",
+      type: "list",
+      message: "Please choose one of the following:",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employees",
+        // "Add Department",
+        // "Add Role",
+        // "Update Employee Role",
+        // "Exit"
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.menu) {
+        case "View All Employees":
+          employeeView();
+          break;
+
+        case "View All Departments":
+          departmentView();
+          break;
+
+        case "View All Roles":
+          roleView();
+          break;
+
+        case "Add Employees":
+          addEmployee();
+          break;
+
+        case "Exit":
+          exit();
+          break;
+      }
+    });
+};
+
+// Function that SELECTS ALL FROM role in our schema.sql/mysql workbench.
+// Connection.query helps us to connect the query
+// This will go for all of the functions created
 
 function employeeView() {
-  connection.query("SELECT * FROM employee", function(err, res) {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.log("\n");
+    console.table(res);
+    console.log("\n");
+    start();
+  });
+}
+
+function departmentView() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.log("\n");
+    console.table(res);
+    console.log("\n");
+    start();
+  });
+}
+
+function roleView() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.log("\n");
+    console.table(res);
+    console.log("\n");
+    start();
+  });
+}
+
+function addEmployee() {
+  //empty array to push inputs inside of
+  const roleList = [];
+
+  //empty array to push inputs inside of
+  const managerList = [];
+
+  connection.query("SELECT * FROM employee_tracker_db.employee", function (
+    err,
+    res
+  ) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      roleList.push(res[i].title);
+    }
+
+    connection.query("SELECT * FROM employee_tracker_db.role", function (
+      err,
+      res
+    ) {
       if (err) throw err;
-      console.log("\n");
-      console.table(res);
-      console.log("\n");
-      runSearch();
-  })
-};
+
+      for (var i = 0; i < res.length; i++) {
+        managerList.push(res[i].first_name + " " + res[i].last_name);
+      }
+
+      // Build out the inquirer prompt to add employee's first name and last name
+
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "firstName",
+          message: "What is the Employee's first name?",
+        },
+        {
+          type: "input",
+          name: "lastName",
+          message: "What is the Employee's last name?",
+        },
+        {
+          type: "input",
+          name: "role",
+          message: "What is the Employee's Role ID?",
+          choices: roleList
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the Employee's Manager?",
+          choices: managerList
+        }
+      ])
+      .then(val => {
+        connection.query(
+            "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, (select id from role where title = ?), (select id from  (SELECT * FROM employee) AS A where id = (select id from  (SELECT * FROM employee) AS A where concat(first_name, ' ', last_name) = ?)));", [
+                val.firstName,
+                val.lastName,
+                val.role,
+                val.managerId
+            ],
+            function(err, res) {
+                if (err) throw err;
+                console.log("\n");
+                console.log("Successfully added Employee");
+                console.log("\n");
+                start();
+            }
+        );
+    });
+})
+})
+}
 
 function exit() {
   connection.end();
